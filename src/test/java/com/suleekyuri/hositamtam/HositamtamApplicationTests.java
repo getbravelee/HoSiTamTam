58f186eac296ad4858b0d9982dc2776bb9e15c57
getbravelee <getbravelee@gmail.com>
2024-11-18 14:35:15 +0900

    init

---
+++ src/test/java/com/suleekyuri/hositamtam/HositamtamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suleekyuri.hositamtam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HositamtamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
