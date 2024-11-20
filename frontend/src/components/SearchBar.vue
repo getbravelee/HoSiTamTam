4b75240d211809f3662ea3c1fa0654780c6236c2
getbravelee <getbravelee@gmail.com>
2024-11-20 20:44:51 +0900

    feat: add 검색 debounce

--- frontend/src/components/SearchBar.vue
+++ frontend/src/components/SearchBar.vue
@@ -1,11 +1,7 @@
 <template>
   <div>
-    <input
-        type="text"
-        v-model="query"
-        @input="fetchSuggestions"
-        placeholder="검색어 입력"
-    />
+    <label for="search-input">Search:</label>
+    <input type="text" id="search-input" v-model="query" @keyup="onKeyUp" placeholder="검색어 입력" />
     <table v-if="suggestions.length" border="1">
       <thead>
       <tr>
@@ -25,17 +21,19 @@
 
 <script>
 import axios from 'axios';
+import debounce from 'lodash/debounce'; // lodash의 debounce 함수 임포트
 
 export default {
   data() {
     return {
       query: '',
-      suggestions: []
+      suggestions: [],
     };
   },
   methods: {
-    fetchSuggestions() {
-      if (this.query.length > 1) {
+    // 디바운스된 함수로 API 호출
+    fetchSuggestions: debounce(function() {
+      if (this.query.length >= 1) {
         axios.get(`/api/search/suggestions?keyword=${this.query}`)
             .then(response => {
               this.suggestions = response.data; // 서버에서 받은 제안 목록
@@ -46,10 +44,22 @@ export default {
       } else {
         this.suggestions = [];
       }
+    }, 300), // 300ms 후에 호출
+
+    // 키 입력 시 호출되는 함수
+    onKeyUp(event) {
+      if (event.key === 'Enter') {
+        this.fetchSuggestions(); // 엔터키 입력 시 즉시 호출
+      } else {
+        this.fetchSuggestions(); // 타이핑이 멈추면 자동 호출
+      }
     },
+
+    // 제안 선택 시 처리
     selectSuggestion(suggestion) {
       this.query = suggestion.locationName; // 선택한 제안으로 검색어 업데이트
       this.suggestions = []; // 제안 목록 비우기
+      this.$router.push(`/region/${suggestion.id}`); // 선택한 동에 해당하는 페이지로 이동
     }
   }
 };
