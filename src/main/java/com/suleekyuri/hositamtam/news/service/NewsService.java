15bb3583ffd3b340ff878af16b7a5f9005ac842e
getbravelee <136793519+getbravelee@users.noreply.github.com>
2024-11-26 09:15:34 +0900

    feat: add newList api

---
+++ src/main/java/com/suleekyuri/hositamtam/news/service/NewsService.java
@@ -0,0 +1,9 @@
+package com.suleekyuri.hositamtam.news.service;
+
+import com.suleekyuri.hositamtam.news.dto.News;
+
+import java.util.List;
+
+public interface NewsService {
+    List<News> getNews();
+}
